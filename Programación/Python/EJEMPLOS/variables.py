eda4da1b508e89f9a8a8a1ee31907488b6d59616
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-09-13 10:39:24 +0200

    Add files via upload

---
+++ Programación/Python/EJEMPLOS/variables.py
@@ -0,0 +1,11 @@
+
+var_entero=4
+var_decimal=3.5
+var_text="hola"
+
+print("el resultado es",var_entero,"manzanas")
+print(var_decimal)
+print(var_text,"como estas?")
+
+var_total=var_entero+var_decimal
+print("el resultado de la suma entre",(var_entero),"y",(var_decimal), "es",var_total)
