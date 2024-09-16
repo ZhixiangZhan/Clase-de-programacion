8408aeae244f5146982ad331420aa15b38d0c231
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-09-16 10:35:01 +0200

    Add files via upload

---
+++ Programación/Python/EJEMPLOS/dias de la semana.py
@@ -0,0 +1,4 @@
+var1=input("escribe el primer dia de la semana:")
+var2=input("escribe el segundo dia de la semana:")
+var3=input("escribe el tercer dia de la semana:")
+print(var3,",",var2,",",var1)
---
+++ Programación/Python/EJEMPLOS/ejemplo imput.py
@@ -0,0 +1,7 @@
+var1=int(input("introduce el primer numero:"))
+var2=int(input("introduce el segundo numero:"))
+
+var_total=var1+var2
+print("el valor introducido es ", var1) 
+print("el segundo valor es ", var2)
+print("la suma del primer numero y el segundo es", var_total)
