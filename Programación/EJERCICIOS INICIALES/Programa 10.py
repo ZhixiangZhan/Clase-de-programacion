26f0978a1b37f5e8ddd376d854d1e2f714eac823
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-09-20 10:46:17 +0200

    Add files via upload

---
+++ Programación/EJERCICIOS INICIALES/Programa 10.py
@@ -0,0 +1,13 @@
+# Introduce por teclado dos números y muestre por pantalla la siguiente información: cociente, resto y si el dividendo es par o impar
+var1=int(input("introduce el valor a dividir"))
+var2=int(input("introduce el dividendo"))
+var_cociente=var1/var2
+var_resto=var1%var2
+
+print(var_cociente)
+print(var_resto)
+if(var_resto=0)
+print("par")
+if not(var_resto!=0)
+print("impar")
+      
---
+++ Programación/EJERCICIOS INICIALES/Programa 9.py
@@ -0,0 +1,6 @@
+# Programa que pida los segundos y muestre por pantalla y en la misma frase los y las horas
+
+var1=int(input("introduzca un numero de segundos:"))
+var_min=var1/60
+var_seg=var1/3600
+print("el numero de minutos es:","{:.2f}".format(var_min),"y el numero de horas es:","{:.4f}".format(var_seg))
