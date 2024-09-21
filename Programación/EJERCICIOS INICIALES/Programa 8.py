1ac8b4fff37aef02dfead247630349aedce77cd3
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-09-21 22:42:43 +0200

    Add files via upload

---
+++ Programación/EJERCICIOS INICIALES/Programa 8.py
@@ -0,0 +1,6 @@
+#Programa que pida un número de horas y muestre por pantalla los minutos y segundos
+
+var1=int(input("Introduzca un número de horas"))
+var_minutos=var1*60
+var_segundos=var1*3600
+print("El numero de minutos es de:",var_minutos,"y el de segundos es:",var_segundos)
