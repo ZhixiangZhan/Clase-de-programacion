39c93e995a3fd073e168a179b3601385b1c736d5
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-09-26 09:16:39 +0200

    Programa 10.py

--- Programación/EJERCICIOS INICIALES/Programa 10.py
+++ Programación/EJERCICIOS INICIALES/Programa 10.py
@@ -1,5 +1,5 @@
 # Introduce por teclado dos números y muestre por pantalla la siguiente información: cociente, resto y si el dividendo es par o impar
-var1=int(input("introduce el valor a dividir"))
+var1=float(input("introduce el valor a dividir"))
 var2=int(input("introduce el dividendo"))
 var_cociente=var1/var2
 var_resto=var1%var2
@@ -8,6 +8,6 @@ print("El cociente es: ","{:.2f}".format(var_cociente))
 print("El resto es: ,"var_resto)
 if var_resto%2==0:
     print("El dividendo es par")
-if var_resto%2==1:
+if False var_resto%2=0:
     print("El dividendo es impar")
 
