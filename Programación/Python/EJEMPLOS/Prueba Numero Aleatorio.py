fa83c8a025d0ee850900ba3f84c895463fe23ee5
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-10-18 09:59:28 +0200

    Prueba Numero Aleatorio.py

--- Programación/Python/EJEMPLOS/Prueba Numero Aleatorio.py
+++ Programación/Python/EJEMPLOS/Prueba Numero Aleatorio.py
@@ -3,9 +3,7 @@ import random
 var1=random.randint(1,100)
 var2=random.randint(1,10)
 var3=var1+var2
-print("var1 es:",var1)
-print("var2 es:",var2)
-respuesta=int(input("introduzca el resultado de (var1)+(var2) :"))
+respuesta=int(input(f"introduzca el resultado de {var1}+{var2} :"))
 if var3==respuesta:
     print("LA RSPUESTA ES CORRECTA")
 if var3!=respuesta:
