42db2c1bd09c4a4e399680a32de57c3843ad7413
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-10-18 10:04:41 +0200

    Programa 23.py

--- Programación/Ejercicios condicionales/Programa 23.py
+++ Programación/Ejercicios condicionales/Programa 23.py
@@ -6,7 +6,8 @@ if var1>10 or var1<0:
 elif var1>8.5:
     print("Has sacado un excelente")
 elif var1<8.5 or var1>6.5:
-    print("Has sacado un excelente")
+    print("Has sacado un notable")
 elif var1<6.5 or var1>5 or var1==5:
-    print("Has aprovado")
-
+    print("Has sacado un satisfactorio")
+elif var1<5:
+   print("Has sacado insuficiente")
