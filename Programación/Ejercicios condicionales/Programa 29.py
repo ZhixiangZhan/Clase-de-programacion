0bc53e674562f9092443feed31f6d8a66150ff46
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-10-28 10:37:21 +0100

    Programa 29.py

--- Programación/Ejercicios condicionales/Programa 29.py
+++ Programación/Ejercicios condicionales/Programa 29.py
@@ -1,3 +1,5 @@
+#29. Busca Internet qué función permite obtener la longitud de un String, realiza un programa 
+que al introducir una frase devuelva la longitud
 var1=input("introduzcan una frase: ")
 len=len(var1)
 print(f"la longitud de la frase es {len}")
