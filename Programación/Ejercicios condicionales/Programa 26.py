5675ac04c678ea0c9294744e5c24f755a7a30e4d
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-10-28 10:29:41 +0100

    Programa 26.py

--- Programación/Ejercicios condicionales/Programa 26.py
+++ Programación/Ejercicios condicionales/Programa 26.py
@@ -1,3 +1,5 @@
+#26. Realiza un programa que, al introducir una letra por teclado, aparezca por pantalla si 
+está o no en mayúscula. Utiliza dos IF’s que establezcan True o False a la condición.
 var1=input("introduzca una letra: ")
 x=var1.islower()
 
