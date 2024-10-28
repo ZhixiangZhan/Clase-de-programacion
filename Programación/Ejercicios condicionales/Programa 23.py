8b1fced9336bbc92ecb1d31ce47a7664cae52669
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-10-28 10:28:29 +0100

    Programa 23.py

--- Programación/Ejercicios condicionales/Programa 23.py
+++ Programación/Ejercicios condicionales/Programa 23.py
@@ -1,3 +1,6 @@
+#23. Modifica el programa anterior para establecer si la nota es un excelente (8.5 a 10), un 
+notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota 
+introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos
 var1=float(input("introduzca la nota: "))
 
    
