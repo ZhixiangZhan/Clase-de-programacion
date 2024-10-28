c5b1974d93ee0d507ee6387a6777d5414f54eeb6
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2024-10-28 10:27:39 +0100

    Programa 22.py

--- Programación/Ejercicios condicionales/Programa 22.py
+++ Programación/Ejercicios condicionales/Programa 22.py
@@ -1,3 +1,5 @@
+#22. Programa que al introducir una nota por teclado te diga si has aprobado o suspendido. 
+Si la nota es menos de un 5 es suspenso y si la nota es 5 o mayor estás aprobado.
 var1=float(input("introduzca la nota: "))
 
    
