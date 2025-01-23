3556375e3e998057e39f5123e2bd22bc67286e59
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2025-01-23 09:11:38 +0100

    Add files via upload

--- Programación/Ejercicios condicionales/Programa 62.py
+++ Programación/Ejercicios condicionales/Programa 62.py
@@ -1,23 +1,26 @@
 var1=int(input("Introduzca un numero: "))
 var2=int(input("Introduzca un numero: "))
-pares=0
-impares=0
+pares=""
+impares=""
 
 if var1<var2:
     contador=var1
     for x in range(var1,var2):
         if contador%2==0:
-            pares=pares+str(x)
+            pares+=str(x)+"-"
         else:
-            impares=impares+""
+            impares+=str(x)+"-"
         contador+=1    
 if var1>var2:
     contador=var2        
-    for contador in range(var2,var1):
+    for x in range(var2,var1):
         if contador%2==0:
-            pares+=str(contador)
+            pares+=str(x)+"-"
         else:
-            impares+=str(contador)
-        contador+=1 
-print(pares,end="-")
-print(impares,end="-")
+            impares+=str(x)+"-"
+        contador+=1
+        
+pares=pares[0:-1]
+impares=impares[0:-1]
+print(pares)
+print(impares)
