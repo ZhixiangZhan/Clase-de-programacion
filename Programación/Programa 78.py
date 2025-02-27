f44b52deefb2a7fe87847d0c24f1fc3f9e495a75
ZhixiangZhan <zhixiang.zhan@campus.xaloc.org>
2025-02-27 09:17:15 +0100

    Programa 78.py

--- Programación/Programa 78.py
+++ Programación/Programa 78.py
@@ -1,7 +1,20 @@
 lista1=["a","b","D","x","r","X","3","h","w","2","i"]
 var1=""
-eliminar=input("introduzca un valor numerico que quiera eliminar: ")
-while var1=="s"
-if eliminar.isnumeric()==True:
+
+while True:
+    eliminar=input("introduzca un valor numerico que quiera eliminar: ")
+    if eliminar.isnumeric():
+        if eliminar not in lista1:
+            print("El valor introducido no está en la lista")
+        else:
+            lista1.remove(eliminar)
+            print(lista1)
+    else:
+        print("introduzca un valor numerico")
     
+    var1=input("Deseas introducir otro valor s/n: ")
+    if var1 == "n":
+        break
+
+
     
